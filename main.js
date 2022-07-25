19b6b4541eb970cdf9ebb7ce0938389d753c007b
Vlad Holub <emokramer@gmail.com>
2022-07-26 02:44:46 +0300

    task2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// task 1
 let varrible1 = "true";
 let varrible2 = "false";
 let varrible3 = 17;
@@ -9,3 +11,12 @@ console.log(typeof varrible2);
 console.log(typeof varrible3);
 console.log(typeof varrible4);
 console.log(typeof varrible5);
+
+// task 2
+let height = 15;
+let width = 20;
+    if(height>width){
+        console.log(height);
+    } else {
+        console.log(width);
+    }
