dfa00d41e1c59dbc97952f063927f106dd09942b
Vlad Holub <emokramer@gmail.com>
2022-07-26 03:16:12 +0300

    task 4

--- main.js
+++ main.js
@@ -29,3 +29,12 @@ while (i < 21) {
     }
     i++;
 }
+
+// task 4
+let key = true;
+let documents = true;
+let pen = true;
+let apple = false;
+let orange = true;
+let shouldGoToWork = key && documents && pen && (apple || orange);
+console.log(shouldGoToWork);
